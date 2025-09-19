b90fe8bc28933f732d4c36f18963bba049870c85
GerardAsuncion <asunciongerard90@gmail.com>
2025-09-19 20:07:36 +0200

    fix: header now works as a link to return to main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
-import { useLocation, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import type { RootState } from '../store/store'
+import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const location = useLocation();
   const { id } = useParams<{ id: string }>();
 
-  const movies = useSelector((state: RootState) => state.movies.items);
+	const isMoviePage = Boolean(id)
 
   return (
 		<header className="bg-blue-950 text-blue-950 flex flex-col justify-between items-center gap-8 py-10 mb-10 h-40">
 			<div className="px-6 py-4 rounded-2xl bg-gray-100 shadow-xl">
-				<h1 className="text-3xl font-bold tracking-wide">
-					MOVIES DIRECTORY
-				</h1>
+				{isMoviePage ? (
+          <Link to="/" className="text-3xl md:text-4xl font-bold tracking-wide cursor-pointer">
+            MOVIES DIRECTORY
+          </Link>
+        ) : (
+          <h1 className="text-3xl md:text-4xl font-bold tracking-wide">MOVIES DIRECTORY</h1>
+        )}
 			</div>
 		</header>
   );
